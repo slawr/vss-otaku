f0d2b2d475b2e383697da84d0e232b25c3155988
Stephen Lawrence <stephen.lawrence@renesas.com>
2022-03-11 19:29:10 +0000

    vss feeder: misc improvements to ws_server.py template
    Miscellaneous improvements to the web socket server template.
    
    Signed-off-by: Stephen Lawrence <stephen.lawrence@renesas.com>

--- feeders/r-sim-feeder/ws_server.py
+++ feeders/r-sim-feeder/ws_server.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
-# A web socket server to receive VSS data for processing
+# A web socket server to receive VSS data for processing.
+#
+# Processing of the data is deliberately missing so this
+# can quickly be copied as a template or starting point
+# for new work.
 #
 # Copyright (C) 2021 Renesas Electronics Corporation
 #
@@ -8,22 +12,20 @@
 import asyncio
 from websockets import serve
 import json
-from r_sim2vss_translater import translate
-
 
 # simple service configuration
 ws_port = 8765
-ws_ip = 'localhost'
+ws_host = None
 
 
 async def handler(websocket):
     async for message in websocket:
         msg = json.loads(message)
         sim_payload = msg['arg']
-
+#       Do some processing
 
 async def main():
-    async with serve(handler, "localhost", 8765):
+    async with serve(handler, ws_host, ws_port):
         await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
